89e7dd035854e23d64d7d6abb4a56eff1de00c4f
User Name <artdevsemyonov@gmail.com>
2022-12-01 01:45:11 +0300

    Реализовал метод update for posts, обязательно еще раз протестировать API

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -63,7 +63,7 @@ export const remove = async (req, res) => {
             (err, doc) => {
                 if (err) {
                     console.log(err);
-                    res.status(500).json({
+                    return res.status(500).json({
                         message: 'Не удалось удалить статью',
                     });
                 }
@@ -92,7 +92,7 @@ export const create = async (req, res) => {
     try {
         const doc = new PostModel({
             title: req.body.title,
-            text: req.body.text,
+            text: req.body.title,
             imageUrl: req.body.imageUrl,
             tags: req.body.tags,
             user: req.userId,
@@ -105,6 +105,34 @@ export const create = async (req, res) => {
         console.log(err);
         res.status(500).json({
             message: 'Не удалось создать статью',
+        });
+    }
+};
+
+export const update = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        await PostModel.updateOne(
+            {
+                _id: postId,
+            }, 
+            {
+                title: req.body.title,
+                text: req.body.text,
+                imageUrl: req.body.imageUrl,
+                user: req.userId,
+                tags: req.body.tags,
+            },
+        );
+
+        res.json({
+            success: true,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось обновить статью',
         });
     }
 };
--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.remove);
-// app.patch('/posts', PostController.update);
+app.patch('/posts/:id', checkAuth, PostController.update);
 
 app.listen(4444, (err) => {
     if(err) {

d055661b2c9dcac10239ad43694a4082122be1f2
User Name <artdevsemyonov@gmail.com>
2022-11-30 23:34:29 +0300

    Реализовал метод remove for posts

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/auth/me', checkAuth, UserController.getMe);
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, PostController.create);
-// app.delete('/posts', PostController.remove);
+app.delete('/posts/:id', checkAuth, PostController.remove);
 // app.patch('/posts', PostController.update);
 
 app.listen(4444, (err) => {

b91c9a3d62b3b7d498ef861dee5c52bf2082cae9
User Name <artdevsemyonov@gmail.com>
2022-11-30 18:57:53 +0300

    Добавил вывод одного поста getOne for posts

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -16,10 +16,34 @@ export const getOne = async (req, res) => {
     try{
         const postId = req.params.id;
 
-        PostModel.findOneAndUpdate({
-            _id: postId,
-        }, );
+        PostModel.findOneAndUpdate(
+            {
+                _id: postId,
+            },
+            {
+                $inc: { viewsCount: 1 },
+            },
+            {
+                returnDocument: 'after',
+            },
+            (err, doc) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).json({
+                        message: 'Не удалось вернуть статью',
+                    });
+                }
 
+                if (!doc) {
+                    console.log(err);
+                    return res.status(404).json({
+                        message: 'Статья не найдена',
+                    });
+                }
+
+                res.json(doc);
+            },
+        );
     } catch (err) {
         console.log(err);
         res.status(500).json({

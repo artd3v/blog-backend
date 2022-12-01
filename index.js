27eabc9aa21dd9c94fb60fb87d2c27e2c4f26efb
User Name <artdevsemyonov@gmail.com>
2022-12-01 05:08:49 +0300

    Добавил возможность загрузки пока ток изображений, но можно расширить, обязательно еще раз протестировать API

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import multer from 'multer';
+
 import mongoose from 'mongoose';
 
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { registerValidation, loginValidation, postCreateValidation} from './validations.js';
+
 import checkAuth from './utils/checkAuth.js';
 
 import * as UserController from './controllers/UserController.js';
@@ -18,18 +21,35 @@ mongoose
 
 const app = express();
 
-app.use(express.json());
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
 
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login',loginValidation, UserController.login);
 app.post('/auth/register', registerValidation, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
 
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    });
+});
+
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.remove);
-app.patch('/posts/:id', checkAuth, PostController.update);
+app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update);
 
 app.listen(4444, (err) => {
     if(err) {

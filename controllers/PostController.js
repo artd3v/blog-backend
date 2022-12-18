b320e3077eb1786dd5117b438715f01aca66ad9c
User Name <artdevsemyonov@gmail.com>
2022-12-19 02:46:06 +0300

    Добавил получение тэгов

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,20 @@
 import PostModel from '../models/Post.js'
 
+export const getLastTags = async (req, res) => {
+    try{
+        const posts = await PostModel.find().limit(5).exec();
+
+        const tags = posts.map((obj) => obj.tags).flat().slice(0, 5);
+
+        res.json(tags)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось получить список тэгов',
+        });
+    }
+};
+
 export const getAll = async (req, res) => {
     try{
         const posts = await PostModel.find().populate('user').exec();
@@ -55,7 +70,7 @@ export const create = async (req, res) => {
     try {
         const doc = new PostModel({
             title: req.body.title,
-            text: req.body.title,
+            text: req.body.text,
             imageUrl: req.body.imageUrl,
             tags: req.body.tags,
             user: req.userId,
--- validations.js
+++ validations.js
@@ -16,6 +16,6 @@ export const registerValidation = [
 export const postCreateValidation = [
     body('title', 'Введите заголовок статьи').isLength({ min: 3 }).isString(),
     body('text', 'Введите текст статьи').isLength({ min: 5 }).isString(),
-    body('tags', 'Неверный формат тэгов(укажите массив)').optional().isString(),
+    body('tags', 'Неверный формат тэгов').optional().isString(),
     body('imageUrl', 'Неверная ссылка на изображение').optional().isString(),
 ];
